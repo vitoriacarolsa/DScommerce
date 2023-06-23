c183114340c73ee525b0087b86363ae71d94a075
visantos <vitoria.santos@confitec.com.br>
2023-06-23 00:10:48 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/devSuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devSuperior/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.devSuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 @Entity
 @Table(name = "tb_product")
@@ -22,6 +23,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
 
     private Set<Category>categories=new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem>items =new HashSet<>();
     public Product(){
     }
 
@@ -76,4 +80,12 @@ public class Product {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
+    }
+
 }
