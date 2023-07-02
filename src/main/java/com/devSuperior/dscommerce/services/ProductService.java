8f204a75bfe1f2c8bb2b060bfa9cc945dcc773c5
visantos <vitoria.santos@confitec.com.br>
2023-07-02 02:14:57 -0300

    GET products paged

--- src/main/java/com/devSuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devSuperior/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devSuperior.dscommerce.dto.ProductDTO;
 import com.devSuperior.dscommerce.entities.Product;
 import com.devSuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,12 +18,16 @@ public class ProductService {
     private ProductRepository repository;
     @Transactional (readOnly = true)
     public ProductDTO findById(Long id){
-        Optional<Product> result= repository.findById(id);
-        Product product = result.get();
-        ProductDTO dto= new ProductDTO(product);
-        return dto;
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+    }
 
+    @Transactional (readOnly = true)
+    public Page <ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x->new ProductDTO(x));
+    }
 
     }
 
-}
+
