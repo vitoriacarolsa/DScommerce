78e17183a38d4306c70aa629584864061a04b518
visantos <vitoria.santos@confitec.com.br>
2023-07-04 23:32:38 -0300

    Validação com Bean Validation

--- src/main/java/com/devSuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devSuperior/dscommerce/controllers/ProductController.java
@@ -6,6 +6,7 @@ import com.devSuperior.dscommerce.entities.Product;
 import com.devSuperior.dscommerce.repositories.ProductRepository;
 import com.devSuperior.dscommerce.services.ProductService;
 import com.devSuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -37,14 +38,14 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity <ProductDTO> insert(@RequestBody ProductDTO dto){
+    public ResponseEntity <ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
     dto= service.insert(dto);
     URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
             .buildAndExpand(dto.getId()).toUri();
     return ResponseEntity.created(uri).body(dto);
     }
     @PutMapping (value= "/{id}")
-    public ResponseEntity<ProductDTO> update (@PathVariable long id, @RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> update (@PathVariable long id,@Valid @RequestBody ProductDTO dto){
        dto= service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
--- src/main/java/com/devSuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devSuperior/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,18 @@
 package com.devSuperior.dscommerce.dto;
 
 import com.devSuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min=3, max=80, message= "Nome precisa ter de 3 a 80 caracteres. " )
+    @NotBlank (message="campo requerido ")
     private String name;
+    @Size(min=10, message= "Descrição precisa ter no mínimo 10 caracteres. " )
+    @NotBlank (message="campo requerido ")
     private String description;
+    @Positive(message="O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
