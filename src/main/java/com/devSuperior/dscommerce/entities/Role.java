ee985002cc75600209636be2d9170530c679c23e
visantos <vitoriacarolinesantos338@gmail.com>
2023-11-07 19:38:19 -0300

    UserDetails, GrantedAuthority

--- src/main/java/com/devSuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devSuperior/dscommerce/entities/Role.java
@@ -1,12 +1,15 @@
 package com.devSuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
+
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,14 +30,13 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
-
     public void setAuthority(String authority) {
         this.authority = authority;
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
