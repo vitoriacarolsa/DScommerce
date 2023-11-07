e8a5dfc1f83259f90e7d38d866d779c15c6bb572
visantos <visantos@brasilseg.com.br>
2023-11-07 17:20:05 -0300

    Domain model User Role, seed

--- src/main/java/com/devSuperior/dscommerce/entities/User.java
+++ src/main/java/com/devSuperior/dscommerce/entities/User.java
@@ -3,8 +3,7 @@ package com.devSuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -23,7 +22,11 @@ public class User {
 
 	@OneToMany (mappedBy = "client")
 	private List <Order> orders =new ArrayList<>();
-
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	public User(){
 	}
 
@@ -87,4 +90,34 @@ public class User {
 	public List<Order> getOrders() {
 		return orders;
 	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(role);
+	}
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
 }
