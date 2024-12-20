4a01895e190101399c2be39391f1cbd10f1316d4
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2024-12-20 11:46:58 -0300

    test : busca de categorias por Id

---
+++ src/test/java/com/devSuperior/dscommerce/tests/CategoryFactory.java
@@ -0,0 +1,16 @@
+package com.devSuperior.dscommerce.tests;
+
+import com.devSuperior.dscommerce.entities.Category;
+
+public class CategoryFactory {
+
+    public static Category createCategory(){
+        return new Category(1L, "Games");
+    }
+
+    public static Category createCategory(Long id, String name){
+        return new Category(id, name);
+    }
+
+
+}
