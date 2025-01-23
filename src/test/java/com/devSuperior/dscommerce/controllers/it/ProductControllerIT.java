56ca8fbc27bf833fc0aaa3213ca635b79b69e0bf
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-01-23 17:50:00 -0300

    ProductControllerIT : teste inserindo produto com dados válidos

--- src/test/java/com/devSuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/devSuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -1,9 +1,17 @@
 package com.devSuperior.dscommerce.controllers.it;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.devSuperior.dscommerce.dto.ProductDTO;
+import com.devSuperior.dscommerce.entities.Category;
+import com.devSuperior.dscommerce.entities.Product;
+import com.devSuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -22,12 +31,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductControllerIT {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private TokenUtil tokenUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+    private String clientToken, adminToken, invalidToken;
+    private Long existingId, nonExistingId, dependentId;
     private String productName;
 
+    private Product product;
+    private ProductDTO productDTO;
+
     @BeforeEach
-    void setUp(){
+    void setUp() throws Exception {
+
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
+
         productName = "Macbook";
 
+        existingId = 2L;
+        nonExistingId = 1000L;
+        dependentId = 3L;
+
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto"; // Simulates a wrong token
+
+        Category category = new Category(2L, null);
+        product = new Product(null, "Console PlayStation 5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit", 3999.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg");
+        product.getCategories().add(category);
+        productDTO = new ProductDTO(product);
     }
     @Test
     public void findAllShouldReturnPageWhenProductNameParamIsEmpty() throws Exception {
@@ -56,5 +96,27 @@ public class ProductControllerIT {
         result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
         result.andExpect(jsonPath("$.content[0].price").value(1250.0));
         result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    @Test
+    public void insertShouldReturnProductDTOCreatedWhenLoggedAsAdmin() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(post("/products")
+                                .header("Authorization", "Bearer " + adminToken)
+                                .content(jsonBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                        .andDo(MockMvcResultHandlers.print());
+
+        result.andExpect(status().isCreated());
+        result.andExpect(jsonPath("$.id").value(26L));
+        result.andExpect(jsonPath("$.name").value("Console PlayStation 5"));
+        result.andExpect(jsonPath("$.description").value("Lorem ipsum dolor sit amet, consectetur adipiscing elit"));
+        result.andExpect(jsonPath("$.price").value(3999.90));
+        result.andExpect(jsonPath("$.imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+        result.andExpect(jsonPath("$.categories[0].id").value(2L));
     }
 }
