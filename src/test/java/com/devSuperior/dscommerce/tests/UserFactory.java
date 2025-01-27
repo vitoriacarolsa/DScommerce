1e1a0f0894070c3b37ee8d4848cca4acd9cc9690
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-01-26 23:22:13 -0300

    teste : OrderControllerIT

--- src/test/java/com/devSuperior/dscommerce/tests/UserFactory.java
+++ src/test/java/com/devSuperior/dscommerce/tests/UserFactory.java
@@ -1,33 +1,36 @@
 package com.devSuperior.dscommerce.tests;
 
+
+
 import com.devSuperior.dscommerce.entities.Role;
 import com.devSuperior.dscommerce.entities.User;
 
 import java.time.LocalDate;
 
 public class UserFactory {
+	
+	public static User createClientUser() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "98888889", LocalDate.parse("2001-07-25"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));
+		return user;
+	}
+	
+	public static User createAdminUser() {
+		User user = new User(2L, "Alex", "alex@gmail.com", "977777777", LocalDate.parse("1987-12-13"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(2L, "ROLE_ADMIN"));		
+		return user;
+	}
+	
+	public static User createCustomClientUser(Long id, String username) {
+		User user = new User(id, "Maria", username, "98888889", LocalDate.parse("2001-07-25"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(1L, "ROLE_CLIENT"));		
+		return user;
+	}
+	
+	public static User createCustomAdminUser(Long id, String username) {
+		User user = new User(id, "Alex", username, "977777777", LocalDate.parse("1987-12-13"), "$2a$10$N7SkKCa3r17ga.i.dF9iy.BFUBL2n3b6Z1CWSZWi/qy7ABq/E6VpO");
+		user.addRole(new Role(2L, "ROLE_ADMIN"));
+		return user;
+	}	
 
-    public static User createClienteUser() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "988118822", LocalDate.parse("2001-07-25"), "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
-        user.addRole(new Role(1L, "ROLE_CLIENT"));
-        return user;
-    }
-
-    public static User createAdminUser() {
-        User user = new User(2L, "Alex", "alex@gmail.com", "988118822", LocalDate.parse("2001-07-25"), "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
-        user.addRole(new Role(2L, "ROLE_ADMIN"));
-        return user;
-    }
-
-    public static User createCustomAdminUser(Long id, String username){
-        User user = new User(id, "Alex", username, "988282818", LocalDate.parse("1987-11-11"), "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
-        user.addRole(new Role(id, "ROLE_ADMIN"));
-        return user;
-    }
-
-    public static User createCustomClientUser(Long id, String username){
-        User user = new User(id, "Maria", username, "988118822", LocalDate.parse("2001-07-25"), "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
-        user.addRole(new Role(1L, "ROLE_CLIENT"));
-        return user;
-    }
 }
